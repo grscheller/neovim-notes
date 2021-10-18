0c88ea4f2946c3bf9302b0693af8eadd3d65832e
grscheller <geoffrey@scheller.com>
2021-10-18 03:25:12 -0600

    Updated to my latest init.lua

--- init_examples/later_init.lua
+++ init_examples/later_init.lua
@@ -13,29 +13,53 @@
 
      Location Paq stores repos: ~/.local/share/nvim/site/pack/paqs/start/ ]]
 require'paq' {
-  "savq/paq-nvim";  -- Paq manages itself
-  "nvim-telescope/telescope.nvim";  -- fuzzy finder over lists
-  "nvim-lua/plenary.nvim";          -- required by telescope.nvim
-  "nvim-lua/popup.nvim";            -- required by telescope.nvim
-  "folke/which-key.nvim";  -- show possible keybinding in popup, also define keybindings
-  "nvim-treesitter/nvim-treesitter";  -- Install language modules for built-in treesitter
-  "neovim/nvim-lspconfig";  -- Collection of common configurations for built-in LSP client
-  "hrsh7th/nvim-compe";  -- Autocompletion framework, can use built-in LSP as a source
-  "L3MON4D3/LuaSnip";  -- Snippet engine, used to handle LSP snippets
-  "simrat39/rust-tools.nvim";  -- extra functionality over rust analyzer
-  "scalameta/nvim-metals";  -- Configure built-in LSP client for Metals Scala language server
-  "norcalli/nvim-colorizer.lua";  -- Colorize hexcodes and names like Blue or Green
-  "folke/tokyonight.nvim";  -- Tokyo Night colorschemes
-  "shadmansaleh/lualine.nvim"  -- Used to configure statusline - fork of hoob3rt/lualine.nvim
+    -- Paq manages itself
+    "savq/paq-nvim";
+
+    -- Colorize hexcodes and names like Blue, Yellow or Green
+    "norcalli/nvim-colorizer.lua";
+
+    -- Tokyo Night colorscheme
+    "folke/tokyonight.nvim";
+
+    -- Statusline - fork of hoob3rt/lualine.nvim
+    "shadmansaleh/lualine.nvim";
+
+    -- define keybindings; show keybindings in popup
+    "folke/which-key.nvim";
+
+    -- Install language modules for built-in treesitter
+    "nvim-treesitter/nvim-treesitter";
+
+    -- Fuzzy finder over lists
+    "nvim-telescope/telescope.nvim";
+    "nvim-lua/plenary.nvim";
+    "nvim-lua/popup.nvim";
+    "sharkdp/fd";
+
+    -- Built-in LSP client config, completion and snippets support
+    "neovim/nvim-lspconfig";
+    "hrsh7th/nvim-cmp";
+    "hrsh7th/cmp-nvim-lsp";
+    "hrsh7th/cmp-path";
+    "hrsh7th/cmp-buffer";
+    "L3MON4D3/LuaSnip";
+    "saadparwaiz1/cmp_luasnip";
+    "rafamadriz/friendly-snippets";
+
+    -- Extra functionality over rust analyzer
+    "simrat39/rust-tools.nvim";
+
+    -- Config built-in LSP client for Metals Scala language server
+    "scalameta/nvim-metals"
 }
 
 --[[ Set some default behaviors ]]
 vim.o.hidden = true  -- Allow hidden buffers
 vim.o.shell = "/bin/sh"  -- Some packages need a POSIX compatible shell
-vim.o.backspace = "indent,eol,start"  -- More powerful backspacing in insert mode
 vim.o.path = vim.o.path .. ".,**"  -- Allow gf and :find to use recursive sub-folders
 vim.o.wildmenu = true                 -- Make tab completion in
-vim.o.wildmode = "longest:full,full"  -- command mode more useful
+vim.o.wildmode = "longest:full,full"  -- command mode more useful.
 
 --[[ Set default encoding, localizations, and file formats ]]
 vim.o.encoding = "utf-8"
@@ -43,11 +67,14 @@ vim.o.fileencoding = "utf-8"
 vim.o.spelllang = "en_us"
 vim.o.fileformats = "unix,mac,dos"
 
+-- Sit do
 --[[ Set default tabstops and replace tabs with spaces ]]
-vim.o.tabstop = 4       -- Tab size 4 spaces
+vim.o.tabstop = 4       -- Display hard tab as 4 spaces
 vim.o.shiftwidth = 4    -- Number of spaces used for auto-indent
-vim.o.softtabstop = 4   -- Number spaces in tab when editing
-vim.o.expandtab = true  -- Expand tabs to spaces when inserting tabs
+vim.o.softtabstop = 4   -- Insert/delete 4 spaces when inserting <Tab>/<BS>
+vim.o.expandtab = true
+
+-- Expand tabs to spaces when inserting tabs
 
 --[[ Settings for LSP client ]]
 vim.o.timeoutlen = 800  -- Milliseconds to wait for key mapped sequence to complete
@@ -74,182 +101,14 @@ vim.o.matchpairs = vim.o.matchpairs .. ",<:>,「:」"  -- Additional matching pa
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.api.nvim_exec([[
-  augroup dynamic_smartcase
-    au!
-    au CmdLineEnter : set nosmartcase
-    au CmdLineEnter : set noignorecase
-    au CmdLineLeave : set smartcase
-    au CmdLineLeave : set ignorecase
-  augroup end
+    augroup dynamic_smartcase
+        au!
+        au CmdLineEnter : set nosmartcase
+        au CmdLineEnter : set noignorecase
+        au CmdLineLeave : set ignorecase
+        au CmdLineLeave : set smartcase
+    augroup end
 ]], false)
-
---[[ Toggle between 3 line numbering states ]]
-vim.o.number = false
-vim.o.relativenumber = false
-
-MyLineNumberToggle = function()
-  if vim.o.relativenumber == true then
-    vim.o.number = false
-    vim.o.relativenumber = false
-  elseif vim.o.number == true then
-    vim.o.number = false
-    vim.o.relativenumber = true
-  else
-    vim.o.number = true
-    vim.o.relativenumber = false
-  end
-end
-
---[[ Setup folke/which-key.nvim ]]
-local wk = require'which-key'
-wk.setup {}
-
--- Normal mode <Leader> keybindings
-vim.g.mapleader = " "
-wk.register {
-  ["<Leader>n"] = {"<Cmd>lua MyLineNumberToggle()<CR>", "Line Number Toggle"},
-  ["<Leader><Space>"] = {"<Cmd>nohlsearch<CR>", "Clear hlsearch"},
-  ["<Leader>sp"] = {"<Cmd>set invspell<CR>", "Toggle Spelling"},
-  ["<Leader>ws"] = {":%s/\\s\\+$//<CR>", "Trim Trailing White Space"},
-  ["<Leader>t"] = {":vsplit<CR>:term fish<CR>i", "Fish Shell in vsplit"},
-  ["<Leader>k"] = {":dig<CR>a<C-K>", "Pick & Enter Diagraph"},
-  ["<Leader>l"] = {":mode<CR>", "Clear & Redraw Screen"},  -- Lost <C-L> for this below
-  -- File related keybindings
-  ["<Leader>f"] = {name = "+file"},
-  ["<Leader>ff"] = {"<Cmd>Telescope find_files<CR>", "Find File"},
-  ["<Leader>fr"] = {"<Cmd>Telescope oldfiles<CR>", "Open Recent File"},
-  ["<Leader>fn"] = {"<Cmd>enew<cr>", "New File"}
-}
-
--- Other normal mode keybindings
-wk.register {
-  -- Fix old vi inconsistency between Y and D & C
-  ["Y"] = {"y$", "Yank to End of Line"},
-  -- Move windows around using CTRL-hjkl in normal mode
-  ["<C-H>"] = {"<C-W>H", "Move Window LHS"},
-  ["<C-J>"] = {"<C-W>J", "Move Window BOT"},
-  ["<C-K>"] = {"<C-W>K", "Move Window TOP"},
-  ["<C-L>"] = {"<C-W>L", "Move Window RHS"},
-  -- Navigate between windows using CTRL+arrow-keys in normal mode
-  ["<C-Left>"]  = {"<C-W>h", "Goto Window Left" },
-  ["<C-Down>"]  = {"<C-W>j", "Goto Window Down" },
-  ["<C-Up>"]    = {"<C-W>k", "Goto Window Up"   },
-  ["<C-Right>"] = {"<C-W>l", "Goto Window Right"},
-  -- Resize windows using ALT-hjkl in normal mode
-  ["<M-h>"] = {"2<C-W><", "Make Window Narrower"},
-  ["<M-j>"] = {"2<C-W>-", "Make Window Shorter" },
-  ["<M-k>"] = {"2<C-W>+", "Make Window Taller"  },
-  ["<M-l>"] = {"2<C-W>>", "Make Window Wider"   }
-}
-
--- Use <Tab> and <S-Tab> to navigate through popup menus
-wk.register({
-  ["<Tab>"]   = {"vim.fn.pumvisible() ? '<C-N>' : '<Tab>'"},
-  ["<S-Tab>"] = {"vim.fn.pumvisible() ? '<C-P>' : '<Tab>'"}
-}, {mode = "i", expr = true})
-
--- Automatically reselect visual area when visual shifting
-wk.register({
-  ["<"] = {"<gv", "Shift left and reselect" },
-  [">"] = {">gv", "Shift right and reselect"}
-}, {mode = "x", expr = true})
-
---[[ Compe for completion ]]
-vim.o.completeopt = "menuone,noselect"
-
-require'compe'.setup {
-  enabled = true;
-  source = {
-    path = true;
-    buffer = true;
-    calc = false;
-    emoji = false;
-    luasnip = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    spell = true;
-    tags = true;
-    treesitter = true
-  }
-}
-
-wk.register({
-  ["<C-Space>"] = {"compe#complete()", "Compe Complete"},
-  ["<CR>"] = {"compe#confirm('<CR>')", "Compe Confirm"},
-  ["<C-E>"] = {"compe#close('<C-E>')", "Compe Close"},
-  ["<C-U>"] = {"compe#scroll({'delta': -4})", "Compe Scroll Up"},
-  ["<C-D>"] = {"compe#scroll({'delta': +4})", "Compe Scroll Down"}
-}, {mode = "i", expr = true, silent = true})
-
-vim.api.nvim_exec([[
-  augroup compe_cursorhold
-    au!
-    au CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
-  augroup end
-]], false)
-
---[[ Setup nvim-treesitter ]]
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'maintained',
-  highlight = {enable = true}
-}
-
---[[ LSP Configurations ]]
-local nvim_lsp = require'lspconfig'
-
-local lsp_servers = {
-  "bashls",         -- Bash-language-server (npm i -g bash-language-server)
-  "pyright",        -- Pyright for Python (pacman or npm)
-  "clangd"
-}
-
-for _, lsp_server in ipairs(lsp_servers) do
-    nvim_lsp[lsp_server].setup {}
-end
-
--- Rust configuration, rust-tools.nvim will call lspconfig itself
-local rust_opts = {
-  server = {}  -- Options to be sent to nvim-lspconfig
-}
-require('rust-tools').setup(rust_opts)
-
--- Metals configuration
-vim.g.metals_server_version = '0.10.6-M1'  -- See https://scalameta.org/metals/docs/editors/overview.html
-metals_config = require'metals'.bare_config
-
-metals_config.settings = {showImplicitArguments = true}
-
-vim.api.nvim_exec([[
-  augroup metals_lsp
-    au!
-    au FileType scala,sbt lua require('metals').initialize_or_attach(metals_config)
-  augroup end
-]], false)
-
--- Setup LSP related keybindings
-wk.register({
-  [","]   = {name = "+lsp"},
-  [",g"]  = {name = "+goto"},
-  [",s"]  = {name = "+symbol"},
-  [",w"]  = {name = "+workspace folder"},
-  [",m"]  = {"<Cmd>lua require'metals'.open_all_diagnostics()<CR>", "Metals Diagnostics"},
-  [",F"]  = {"<Cmd>lua vim.lsp.buf.formatting()<CR>", "Formatting"},
-  [",gd"] = {"<Cmd>lua vim.lsp.buf.definition()<CR>", "Goto Definition"},
-  [",gD"] = {"<Cmd>lua vim.lsp.buf.declaration()<CR>", "Goto Declaration"},
-  [",gi"] = {"<Cmd>lua vim.lsp.buf.implementation()<CR>", "Goto Implementation"},
-  [",gr"] = {"<Cmd>lua vim.lsp.buf.references()<CR>", "Goto References"},
-  [",hs"] = {"<Cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help"},
-  [",H"]  = {"<Cmd>lua vim.lsp.buf.hover()<CR>", "Hover"},
-  [",K"]  = {"<Cmd>lua vim.lsp.buf.worksheet_hover()<CR>", "Worksheet Hover"},
-  [",rn"] = {"<Cmd>lua vim.lsp.buf.rename()<CR>", "Rename"},
-  [",sd"] = {"<Cmd>lua vim.lsp.buf.document_symbol()<CR>", "Document Symbol"},
-  [",sw"] = {"<Cmd>lua vim.lsp.buf.workspace_symbol()<CR>", "Workspace Symbol"},
-  [",wa"] = {"<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Workspace Folder"},
-  [",wr"] = {"<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Workspace Folder"},
-  [",l"]  = {"<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", "Diagnostic Set Loclist"},
-  [",["]  = {"<Cmd>lua vim.lsp.diagnostic.goto_prev {wrap = false}<CR>", "Diagnostic Goto Prev"},
-  [",]"]  = {"<Cmd>lua vim.lsp.diagnostic.goto_next {wrap = false}<CR>", "Diagnostic Goto Next"}
-})
 
 --[[ Setup colorsscemes and statusline ]]
 vim.o.termguicolors = true
@@ -261,7 +120,224 @@ vim.g.tokyonight_italic_functions = 1
 vim.g.tokyonight_sidebars = {"qf", "vista_kind", "terminal", "packer"}
 
 require'lualine'.setup {
-  options = {theme = "tokyonight"}
+    options = {theme = "tokyonight"}
 }
 
 vim.cmd[[colorscheme tokyonight]]
+
+--[[ Setup folke/which-key.nvim ]]
+local wk = require'which-key'
+wk.setup {
+    plugins = {
+        spelling = {
+            enabled = true,
+            suggestions = 36
+        }
+    }
+}
+
+-- Normal mode keybindings
+wk.register {
+    ["<Space><Space>"] = {":nohlsearch<CR>", "Clear hlsearch"},
+    ["Y"] = {"y$", "Yank to End of Line"}, -- Fix between Y, D & C inconsistency
+    ["gp"] = {"`[v`]", "Reselect Previous Changed Text"},
+    ["<Space>sp"] = {":set invspell<CR>", "Toggle Spelling"},
+    ["<Space>ws"] = {":%s/\\s\\+$//<CR>", "Trim Trailing White Space"},
+    ["<Space>t"] = {":vsplit<CR>:term fish<CR>i", "Fish Shell in vsplit"},
+    ["<Space>k"] = {":dig<CR>a<C-K>", "Pick & Enter Diagraph"},
+    ["<Space>l"] = {":mode<CR>", "Clear & Redraw Screen"},  -- Lost <C-L> for this below
+    ["<Space>b"] = {":enew<CR>", "New Unnamed Buffer"},
+    -- Telescope related keybindings
+    ["<Space>f"] = {name = "+Telescope"},
+    ["<Space>fb"] = {":Telescope buffers<CR>", "Buffers"},
+    ["<Space>ff"] = {":Telescope find_files<CR>", "Find File"},
+    ["<Space>fg"] = {":Telescope live_grep<CR>", "Live Grep"},
+    ["<Space>fh"] = {":Telescope help_tags<CR>", "Help Tags"},
+    ["<Space>fr"] = {":Telescope oldfiles<CR>", "Open Recent File"},
+    -- Move windows around using CTRL-hjkl
+    ["<C-H>"] = {"<C-W>H", "Move Window LHS"},
+    ["<C-J>"] = {"<C-W>J", "Move Window BOT"},
+    ["<C-K>"] = {"<C-W>K", "Move Window TOP"},
+    ["<C-L>"] = {"<C-W>L", "Move Window RHS"},
+    -- Navigate between windows using CTRL+arrow-keys
+    ["<M-Left>"]  = {"<C-W>h", "Goto Window Left" },
+    ["<M-Down>"]  = {"<C-W>j", "Goto Window Down" },
+    ["<M-Up>"]    = {"<C-W>k", "Goto Window Up"   },
+    ["<M-Right>"] = {"<C-W>l", "Goto Window Right"},
+    -- Resize windows using ALT-hjkl for Linux
+    ["<M-h>"] = {"2<C-W><", "Make Window Narrower"},
+    ["<M-j>"] = {"2<C-W>-", "Make Window Shorter" },
+    ["<M-k>"] = {"2<C-W>+", "Make Window Taller"  },
+    ["<M-l>"] = {"2<C-W>>", "Make Window Wider"   }
+}
+
+--[[ Toggle between 3 line numbering states ]]
+vim.o.number = false
+vim.o.relativenumber = false
+
+myLineNumberToggle = function()
+    if vim.o.relativenumber == true then
+        vim.o.number = false
+        vim.o.relativenumber = false
+    elseif vim.o.number == true then
+        vim.o.number = false
+        vim.o.relativenumber = true
+    else
+        vim.o.number = true
+        vim.o.relativenumber = false
+    end
+end
+
+-- Normal mode keybindings
+wk.register {
+    ["<Space>n"] = {":lua myLineNumberToggle()<CR>", "Line Number Toggle"}
+}
+
+--[[ Setup nvim-treesitter ]]
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = 'maintained',
+    highlight = {enable = true}
+}
+
+--[[ nvim-cmp for completions ]]
+vim.o.completeopt = "menuone,noinsert,noselect"
+
+local myHasWordsBefore = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require'luasnip'
+local cmp = require'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    mapping = {
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        ['<C-E>'] = cmp.mapping.close(),
+
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        },
+
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif myHasWordsBefore() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {"i", "s"}),
+
+        ['<C-D>'] = cmp.mapping.scroll_docs(-4),
+
+        ['<C-F>'] = cmp.mapping.scroll_docs(4)
+    },
+    sources = {
+        {name = 'nvim_lsp'},
+        {name = 'luasnip'},
+        {name = 'nvim_lua'},
+        {name = 'path'},
+        {
+            name = 'buffer',
+            opts = {
+                get_bufnrs = function()
+                    return vim.api.nvim_list_bufs()
+                end
+            }
+        },
+        {name = 'treesitter'}
+    }
+}
+
+--[[ LSP Configurations ]]
+local nvim_lsp = require'lspconfig'
+
+local lsp_servers = {
+    "bashls", -- Bash-language-server (pacman or sudo npm i -g bash-language-server)
+    "clangd", -- C and C++ - both clang and gcc
+    "html",   -- HTML (npm i -g vscode-langservers-extracted)
+    "pyright" -- Pyright for Python (pacman or npm)
+}
+
+for _, lsp_server in ipairs(lsp_servers) do
+    nvim_lsp[lsp_server].setup {
+        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    }
+end
+
+--[[ Rust configuration, rust-tools.nvim will call lspconfig itself ]]
+local rust_opts = {
+    tools = {
+        autoSetHints = true,
+        hover_with_actions = true,
+        inlay_hints ={
+            show_parameter_hints = false,
+            parameter_hints_prefix = "",
+            other_hints_prefix = ""
+        },
+    },
+    -- Options to be sent to nvim-lspconfig
+    -- overriding defaults set by rust-tools.nvim
+    server = {
+        settings = {}
+    }
+}
+
+require('rust-tools').setup(rust_opts)
+
+--[[ Metals configuration ]]
+vim.g.metals_server_version = '0.10.7'  -- See https://scalameta.org/metals/docs/editors/overview.html
+metals_config = require'metals'.bare_config()
+
+metals_config.settings = {showImplicitArguments = true}
+
+vim.api.nvim_exec([[
+    augroup metals_lsp
+        au!
+        au FileType scala,sbt lua require('metals').initialize_or_attach(metals_config)
+    augroup end
+]], false)
+
+--[[ LSP related keybindings ]]
+wk.register {
+    [","]   = {name = "+lsp"},
+    [",g"]  = {name = "+goto"},
+    [",s"]  = {name = "+symbol"},
+    [",w"]  = {name = "+workspace folder"},
+    [",m"]  = {":lua require'metals'.open_all_diagnostics()<CR>", "Metals Diagnostics"},
+    [",F"]  = {":lua vim.lsp.buf.formatting()<CR>", "Formatting"},
+    [",gd"] = {":lua vim.lsp.buf.definition()<CR>", "Goto Definition"},
+    [",gD"] = {":lua vim.lsp.buf.declaration()<CR>", "Goto Declaration"},
+    [",gi"] = {":lua vim.lsp.buf.implementation()<CR>", "Goto Implementation"},
+    [",gr"] = {":lua vim.lsp.buf.references()<CR>", "Goto References"},
+    [",hs"] = {":lua vim.lsp.buf.signature_help()<CR>", "Signature Help"},
+    [",H"]  = {":lua vim.lsp.buf.hover()<CR>", "Hover"},
+    [",K"]  = {":lua vim.lsp.buf.worksheet_hover()<CR>", "Worksheet Hover"},
+    [",rn"] = {":lua vim.lsp.buf.rename()<CR>", "Rename"},
+    [",sd"] = {":lua vim.lsp.buf.document_symbol()<CR>", "Document Symbol"},
+    [",sw"] = {":lua vim.lsp.buf.workspace_symbol()<CR>", "Workspace Symbol"},
+    [",wa"] = {":lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Workspace Folder"},
+    [",wr"] = {":lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Workspace Folder"},
+    [",l"]  = {":lua vim.lsp.diagnostic.set_loclist()<CR>", "Diagnostic Set Loclist"},
+    [",["]  = {":lua vim.lsp.diagnostic.goto_prev {wrap = false}<CR>", "Diagnostic Goto Prev"},
+    [",]"]  = {":lua vim.lsp.diagnostic.goto_next {wrap = false}<CR>", "Diagnostic Goto Next"}
+}
